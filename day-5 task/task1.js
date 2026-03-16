dc6818e595fb471626d534be9cfe14c0b155f44f
shreya <shreyashetty205@gmail.com>
2026-03-16 21:55:33 +0530

    my assignments

---
+++ day-5 task/task1.js
@@ -0,0 +1,7 @@
+let name = "Nishan";
+let age = 22;
+let favColor = "Black";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Color:", favColor);
---
+++ sgnupform react/src/App.jsx
@@ -0,0 +1,13 @@
+import SignupForm from "./components/SignupForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Smart Signup Form</h1>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
